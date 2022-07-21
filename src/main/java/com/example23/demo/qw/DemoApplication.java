59b96b2d676c58cc10179b5df6271ad07f0137a0
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2022-07-21 17:44:34 +0530

    commit of docker with maven

---
+++ src/main/java/com/example23/demo/qw/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example23.demo.qw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/")
+	public String message(){
+		return "welcome chachaaaaa................";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
